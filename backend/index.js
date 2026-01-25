78521bfaf7091b0c4511b9c9f631928ddf474602
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2026-01-25 11:21:46 +0530

    Defining prediction routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ app.get('/',(req,res)=>{
 
 })
 
+app.post('/predict',(req,res)=>{
+    const {meetings_per_day,total_minutes,break_minutes,role,meeting_type}=req.body
+    
+})
+
 app.listen(3000,()=>{
     console.log("Server is running")
 
